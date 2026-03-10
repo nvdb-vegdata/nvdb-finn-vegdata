9a91dd7035e77feb91d0cbff7bbc600d18c25cf9
Geir Sagberg <geir.sagberg@vegvesen.no>
2026-03-10 19:10:19 +0100

    Add iktlosning

--- deploy-atlas.sh
+++ deploy-atlas.sh
@@ -6,6 +6,7 @@ ENV_NAME="${1:?$USAGE}"
 TAG="${2:?$USAGE}"
 RESPONSIBLE_USER="${3:-}"
 DEPLOY="nvdb-finn-vegdata"
+IKT_LOSNING="nvdbapil"
 
 FILE="atlas/$ENV_NAME/$DEPLOY.json"
 BACKUP="$FILE.bak.$(date +%s)"
@@ -30,8 +31,8 @@ mv -f "$TMP" "$FILE"
 (
   cd atlas
   if [[ -n "$RESPONSIBLE_USER" ]]; then
-    ac deploy -e "$ENV_NAME" "$DEPLOY" --block-until-finished --responsible-user "$RESPONSIBLE_USER"
+    ac deploy -e "$ENV_NAME" -i "$IKT_LOSNING" "$DEPLOY" --block-until-finished --responsible-user "$RESPONSIBLE_USER"
   else
-    ac deploy -e "$ENV_NAME" "$DEPLOY" --block-until-finished
+    ac deploy -e "$ENV_NAME" -i "$IKT_LOSNING" "$DEPLOY" --block-until-finished
   fi
 )
