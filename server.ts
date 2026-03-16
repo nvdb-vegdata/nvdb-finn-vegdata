606fa946f80617586d1a28bd26b0f19b56e1b9ca
Geir Sagberg <geir.sagberg@vegvesen.no>
2026-03-16 12:59:48 +0100

    Try other ports

--- server.ts
+++ server.ts
@@ -1,14 +1,28 @@
 import index from './index.html'
 
-Bun.serve({
-  port: 3000,
-  routes: {
-    '/': index,
-  },
-  development: {
-    hmr: true,
-    console: true,
-  },
-})
+let port = 3000
 
-console.log('Server running at http://localhost:3000')
+for (let i = 0; i < 10; i++) {
+  try {
+    Bun.serve({
+      port: port,
+      routes: {
+        '/': index,
+      },
+      development: {
+        hmr: true,
+        console: true,
+      },
+    })
+    console.log(`Server running at http://localhost:${port}`)
+    break
+  } catch (error: any) {
+    if ('code' in error && error.code === 'EADDRINUSE') {
+      console.warn(`Port ${port} is in use, trying next port...`)
+      port++
+    } else {
+      console.error('Failed to start server:', error)
+      process.exit(1)
+    }
+  }
+}
